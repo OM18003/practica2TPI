dc8d7e030446c44b747ac7da9989ba5c2a4b5957
cmargueiz <c.margueiz@gmail.com>
2021-03-16 14:56:59 -0600

    definicion de propiedades y metodos

--- app/Http/Controllers/CirculoController.php
+++ app/Http/Controllers/CirculoController.php
@@ -6,7 +6,17 @@ use Illuminate\Http\Request;
 
 class CirculoController extends Controller
 {
+    private double radio;
+    private double centroX;
+    private double centroY;
+
+
     public function area(Request $request){
+        
+    }
+
+    public function perimetro(Request $request)
+    {
         
     }
 }
