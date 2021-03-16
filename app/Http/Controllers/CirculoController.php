7ae1a377f87389015e3140ac52cf8fa946678aa8
cmargueiz <c.margueiz@gmail.com>
2021-03-16 15:33:10 -0600

    construccion de cuerpo de pruebas

--- app/Http/Controllers/CirculoController.php
+++ app/Http/Controllers/CirculoController.php
@@ -10,13 +10,18 @@ class CirculoController extends Controller
     private $centroX;
     private $centroY;
 
-
-    public function area(){
-        return 2;        
+    public function __construct($radio,$centroX,$centroY) {
+        $this->$radio = $radio;
+        $this->$centroX = $centroX;
+        $this->$centroY = $centroY;
     }
 
-    public function perimetro(Request $request)
+    public function area(){
+        return (($this->radio)**2)*pi();
+    }
+
+    public function perimetro()
     {
-        
+        return 2*(pi())**$this->radio;
     }
 }
