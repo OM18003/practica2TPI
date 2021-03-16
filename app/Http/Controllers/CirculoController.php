8e2dafca69a1e5f8aa5dfd8f754140e5133098ab
mm18018 <mm18018@ues.edu.sv>
2021-03-16 14:43:34 -0600

    Controlador

---
+++ app/Http/Controllers/CirculoController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CirculoController extends Controller
+{
+    public function area(Request $request){
+        
+    }
+}
