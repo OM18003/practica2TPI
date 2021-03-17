26a76bffc1773ad2fe9c8ed99d60c26326a47cad
mm18018 <mm18018@ues.edu.sv>
2021-03-16 18:11:41 -0600

    Tests corregiddos

--- tests/Feature/CirculoControllerTest.php
+++ tests/Feature/CirculoControllerTest.php
@@ -1,31 +1,37 @@
 <?php
 
-namespace Tests\Unit;
+namespace Tests\Feature;
 
-use PHPUnit\Framework\TestCase;
 use App\Http\Controllers\CirculoController;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
 
 class CirculoControllerTest extends TestCase
 {
     private $circulo;
 
-    public function __construct() {
-        $this->circulo = new CirculoController(2,2,2);
-    }
+    // public function __construct()
+    // {
+    //     $this->circulo = new CirculoController(0.7978845608, 2, 2);
+    // }
     /**
      * A basic unit test example.
      *
      * @return void
      */
     public function test_area()
-    
+
     {
-        $areaResultado = $this->circulo->area();
-        $this->assertTrue($areaResultado==2);
+        $this->circulo = new CirculoController(2, 2, 2);
+        $areaResultado = $this->circulo->area();        
+        $this->assertNotNull($areaResultado);
     }
 
     public function test_perimetro()
     {
-        
+        $this->circulo = new CirculoController(2, 2, 2);
+        $areaResultado = $this->circulo->perimetro();        
+        $this->assertNotNull($areaResultado);
     }
 }
